55ac6a065a7f9d221001cace11e5567b439a8b7b
Matias Jesús Pino Saa <mpino@mtt.cl>
2025-08-01 07:35:14 -0400

    se implemento recepción por codigo 200 OK

--- tienda.py
+++ tienda.py
@@ -18,5 +18,8 @@ def productos():
         resultados = cursor.fetchall()
         return render_template('/productos.html', productos=resultados)
     
+def health_check():
+    return "OK", 200
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
